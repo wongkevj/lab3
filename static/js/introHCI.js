cb6acb8828b7362a8ac8a005e6c7a8bb1fa977e4
RandomDentyne <randomdentyne@gmail.com>
2018-01-25 09:08:50 -0800

    lab3 slide 48

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -11,8 +11,25 @@ $(document).ready(function() {
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron button').text("Yes it is");
+		$("#testjs").toggleClass("active");
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
 }
+
+function projectClick(e) {
+	e.preventDefault();
+	$(this).css("background-color", "#7fff00");
+	var containingProject = $(this).closest(".project");
+	var description = $(containingProject).find(".project-description");
+	if(description.length == 0) {
+		$(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+	}
+	else {
+		description.fadeToggle();
+	}
+}
+
